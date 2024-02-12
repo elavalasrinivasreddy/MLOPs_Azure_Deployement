9f65d58397fad51ab50cf7da46dc7bb0cba66eb0
elavalasrinivasreddy <elavalasrinivasreddy@github.com>
2024-02-12 17:58:46 +0530

    added requirements for development

---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+# echo [$(date)]: "START"
+
+# echo [$(date)]: "Creating venv with python=3.9"
+
+# python -m venv .venv
+
+# echo [$(date)]: "Activating the virtual environment"
+
+# source .venv/Scripts/activate
+
+# echo [$(date)]: "Installing the development requirments"
+
+# python -m pip install -r requirements_dev.txt
+
+# echo [$(date)]: "END"
